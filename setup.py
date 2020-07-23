8f210878e7080250a4defe9081cd8f2e0edc6dda
Christophe Vanlancker <carroarmato0@inuits.eu>
2020-07-23 16:18:41 +0200

    Add .gitignore file
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Parking2Influx',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Christophe Vanlancker',
+    author_email='carroarmato0@gmail.com',
+    description='A script which retreives the real-time parking occupation in Gent.'
+)
