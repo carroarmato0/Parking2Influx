c4dd0103c99e57d098f7e489af71f24e0344359e
Christophe Vanlancker <carroarmato0@inuits.eu>
2020-07-23 18:41:04 +0200

    Fix API endpoint changes and API definitions
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- parking2influx.py
+++ parking2influx.py
@@ -23,19 +23,21 @@ def submit_data(influxdb_ip, database, data):
     payload = []
 
     # Loop through data
-    for parking in data:
+    for parking in data['records']:
         payload.append(
             "{measurement},city={city},name={name},description={description},latitude={latitude},longitude={longitude} "
             "max_capacity={max_capacity},current_capacity={current_capacity},open={is_open}".format(
                 measurement="state",
-                city=parking["city"]["name"],
-                name=parking["name"].replace(" ", "\\ "),
-                description=parking["description"].replace(" ", "\\ "),
-                latitude=parking["latitude"],
-                longitude=parking["longitude"],
-                max_capacity=parking["parkingStatus"]["totalCapacity"],
-                current_capacity=parking["parkingStatus"]["availableCapacity"],
-                is_open=int(parking["parkingStatus"]["open"] == True)))
+                name=parking["fields"]["name"].replace(" ", "\\ "),
+                city=json.loads(parking["fields"]["city"])['name'],
+                description=parking["fields"]["description"].replace(" ", "\\ "),
+                latitude=parking["fields"]["latitude"],
+                longitude=parking["fields"]["longitude"],
+                max_capacity=json.loads(parking["fields"]["parkingstatus"])["totalCapacity"],
+                current_capacity=json.loads(parking["fields"]["parkingstatus"])["availableCapacity"],
+                is_open=int(json.loads(parking["fields"]["parkingstatus"])["open"] == True)
+            )
+        )
 
     client.write_points(payload, batch_size=len(payload), protocol='line')
 
@@ -51,7 +53,8 @@ def resolve_container_address(container):
 
 def main():
     influx_ip = resolve_container_address("influxdb")
-    parking_data = fetch_parking("https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json")
+    parking_data = fetch_parking("https://data.stad.gent/api/records/1.0/search/?dataset=bezetting-parkeergarages"
+                                 "-real-time&q=&facet=description")
     submit_data(influx_ip, "parking", parking_data)
 
 
